384279800373adb57a7847d63c4d75069e2879bd
sarrside <side.sarr@uadb.edu.sn>
2020-05-01 16:01:08 +0000

    deploy 5

--- app.js
+++ app.js
@@ -13,4 +13,4 @@ app.get('/', function(req, res){
 });
 app.post('/', userController.create)
 
-app.listen(8080, function(){ console.log('Node server listening on port 8080');});
+app.listen(process.env.PORT || 5000, function(){ console.log(`Node server listening on port ${process.env.PORT || 5000}`);});
