30acce2106c3ec6941dc238081caee4202c1d7b4
sarrside <side.sarr@uadb.edu.sn>
2020-05-02 00:57:35 +0000

    deploy 7

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const userController = require('./app/api/controllers/user');
 // connection to mongodb
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(logger('dev'));
+require('./routes')(app);
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', function(req, res){
  res.json({"tutorial" : "Build REST API with node.js"});
 });
-app.post('/', userController.create)
+
 
 app.listen(process.env.PORT || 5000, function(){ console.log(`Node server listening on port ${process.env.PORT || 5000}`);});
